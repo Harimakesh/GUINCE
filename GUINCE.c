49b1b9ffeb5819619993f4c4885154927cb031a0
Hari Makesh <harimakesh03@gmail.com>
2024-08-10 21:35:46 +0530

    BlackPawnMoveGen

--- GUINCE.c
+++ GUINCE.c
@@ -6,7 +6,9 @@
 #define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
 #define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 #define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
-#define PAWNMOVES "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define PAWNMOVESW "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define PAWNMOVESB "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
+
 
 void ShowSqAtByside(const int side, const S_BOARD *pos) {
 
@@ -38,7 +40,7 @@ int main() {
 
     S_BOARD board[1];
 
-    ParseFen(PAWNMOVES, board);
+    ParseFen(PAWNMOVESB, board);
     PrintBoard(board);
 
     S_MOVELIST list[1];
